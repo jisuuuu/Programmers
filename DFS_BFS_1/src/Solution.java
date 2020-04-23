787b42f39b24ab78538d9d4454d20f2c78d5d6c9
jisuuuu <gjkl55@naver.com>
2020-04-23 21:35:08 +0900

    programmers dfs No.1

--- DFS_BFS_1/src/Solution.java
+++ DFS_BFS_1/src/Solution.java
@@ -1,9 +1,30 @@
 
 public class Solution {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int answer;
+	
+	public void dfs(int[] numbers, int target, int idx){
+        if(idx == numbers.length){
+            int sum = 0;
+            
+            for(int i = 0; i < numbers.length; i++){
+                sum += numbers[i];
+            }
+            
+            if(sum == target)
+                answer++;
+            return;
+        }
+        else{
+            numbers[idx] *= 1;
+            dfs(numbers, target, idx + 1);
+            
+            numbers[idx] *= -1;
+            dfs(numbers, target, idx + 1);
+        }
 	}
-
+	
+	public int solution(int[] numbers, int target) {
+        dfs(numbers, target, 0);
+        return answer;
+    }
 }
