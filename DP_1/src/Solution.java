9b062f7c1da358a0b14749c79f703424f6921581
jisuuuu <gjkl55@naver.com>
2020-06-21 20:59:18 +0900

    dp No.3

--- DP_1/src/Solution.java
+++ DP_1/src/Solution.java
@@ -1,4 +1,14 @@
-
-public class Solution {
-
+//타일 장식물
+class Solution {
+    public long solution(int N) {
+        long[] dp = new long[N];
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i = 2; i < N; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return 2 * (dp[N - 1] + dp[N - 1] + dp[N - 2]);
+    }
 }
