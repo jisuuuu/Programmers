13b062cf4d7802ba16da59e92e0668a95ff0fcb0
jisuuuu <gjkl55@naver.com>
2020-06-26 13:44:33 +0900

    practice 문자열 다루기 기본

---
+++ PRACTICE_1/src/Solution.java
@@ -0,0 +1,16 @@
+//문자열 다루기 기본
+class Solution {
+    public boolean solution(String s) {
+        if(s.length() == 4 || s.length() == 6){
+            for(int i = 0; i < s.length(); i++){
+                char tmp = s.charAt(i);
+                
+                if(Character.isDigit(tmp) == false){
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+}
