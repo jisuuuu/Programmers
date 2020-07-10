a4916f19a585abedf66d637676455db97fd8537e
jisuuuu <gjkl55@naver.com>
2020-07-10 13:40:03 +0900

    PRACTICE_3 자릿수 더하기

--- PRACTICE_2/src/Solution.java
+++ PRACTICE_2/src/Solution.java
@@ -1,4 +1,21 @@
+//정수 내림차순으로 배치하기
+import java.util.*;
 
-public class Solution {
-
+class Solution {
+    public long solution(long n) {
+        String answer = "";
+        String str = String.valueOf(n);
+        List<Character> list = new ArrayList<Character>();
+        
+        for(int i = 0; i < str.length(); i++){
+            list.add(str.charAt(i));
+        }
+        Collections.sort(list);
+        Collections.reverse(list);
+        
+        for(int i = 0; i < list.size(); i++){
+            answer += list.get(i);
+        }
+        return  Long.parseLong(answer);
+    }
 }
